eb6f4ece375bfcccf2485f906a60ea843de3c989
Tooni <113641377+TooniMike@users.noreply.github.com>
2024-06-20 20:56:40 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3960445380"
+    serviceWorkerVersion: "903165758"
   }
 });
